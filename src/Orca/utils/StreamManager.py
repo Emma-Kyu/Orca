f66761552feb4385bb4f14e053f57ba626504f35
Joshua Usi <ExperimentatorsY@gmail.com>
2026-03-25 15:12:47 +1100

    Create StreamManager.py

---
+++ src/Orca/utils/StreamManager.py
@@ -0,0 +1,12 @@
+class StreamManager:
+	def __init__(self):
+		pass
+
+	def open_stream() -> str:
+		pass
+
+	def close_stream(self, stream_id: str):
+		pass
+
+	def get_stream(self, stream_id: str) -> StreamSession | None:
+		pass
